3f3c3eb02856031e1fe26fd6277bddab2b3ae530
Dylanleisner <dleisner@udesa.edu.ar>
2022-11-10 18:57:07 -0300

    condicione busqueda punto2

--- js/genero.js
+++ js/genero.js
@@ -1,3 +1,16 @@
+// buscador con sus condiciones
+let rta_buscador= document.querySelector(".buscador")
+submit.addEventListener('click', function(hola){
+  if (busq.value == '') {
+      alert('No buscó nada.')
+  }
+  else if (busq.value.length <= 3){
+
+      alert('Introduzca como minimo 3 caracteres')
+  }
+})
+
+// Generos
 let generos="https://api.themoviedb.org/3/genre/movie/list?api_key=6b8e258b66583b977b648fcc8df4f960&language=en-US"
 fetch(generos)
     .then(function(response) {
@@ -5,13 +18,12 @@ fetch(generos)
 })
     .then(function(data) {
         console.log(data);
-        console.log(data.genres[0].name)
         let geneross= document.querySelector(".titulopelis")
         let generos1= document.querySelector(".titulopelis1")
         let generos2= document.querySelector(".titulopelis2")
         let generos3= document.querySelector(".titulopelis3")
         let generos4= document.querySelector(".titulopelis4")
-        // let generovacio=""
+        let generovacio=""
         // for (let i=0; i<=3; i++){
         //     generovacio+=data.genres[i].name + ", "
         //     console.log(generovacio)
@@ -19,16 +31,13 @@ fetch(generos)
 
 
         // }
+        // geneross.innerText+=generovacio[0]
         geneross.innerText+= data.genres[0].name
         generos1.innerText+=data.genres[1].name
         generos2.innerText+=data.genres[2].name
         generos3.innerText+=data.genres[3].name
         generos4.innerText+=data.genres[4].name
 
-
-
-
-        
 })
     .catch(function(error) {
   console.log("Error: " + error);
