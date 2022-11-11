2e0f3f0170745e7efdf3fa94f3797307f51beced
Dylanleisner <dleisner@udesa.edu.ar>
2022-11-11 11:27:33 -0300

    buscador y genero terminado

--- js/genero.js
+++ js/genero.js
@@ -1,14 +1,16 @@
-// buscador con sus condiciones
-let rta_buscador= document.querySelector(".buscador")
-submit.addEventListener('click', function(hola){
-  if (busq.value == '') {
-      alert('No buscó nada.')
-  }
-  else if (busq.value.length <= 3){
-
-      alert('Introduzca como minimo 3 caracteres')
-  }
-})
+  // buscador con sus condiciones
+  let buscador= document.querySelector(".buscador")
+  let rtabuscador=document.querySelector(".rta")
+  buscador.addEventListener('submit', function(hola){
+    hola.preventDefault()
+    
+    if (rtabuscador.value == '') {
+        alert('No buscó nada.')
+    }
+    else if (rtabuscador.value.length <= 3){
+        alert('Introduzca como minimo 3 caracteres')
+    }
+  })
 
 // Generos
 let generos="https://api.themoviedb.org/3/genre/movie/list?api_key=6b8e258b66583b977b648fcc8df4f960&language=en-US"
@@ -18,25 +20,25 @@ fetch(generos)
 })
     .then(function(data) {
         console.log(data);
-        let geneross= document.querySelector(".titulopelis")
-        let generos1= document.querySelector(".titulopelis1")
-        let generos2= document.querySelector(".titulopelis2")
-        let generos3= document.querySelector(".titulopelis3")
-        let generos4= document.querySelector(".titulopelis4")
+        let generos= document.querySelector(".primeraLinea")
         let generovacio=""
-        // for (let i=0; i<=3; i++){
-        //     generovacio+=data.genres[i].name + ", "
-        //     console.log(generovacio)
-
-
-
-        // }
+        for (let i=0; i<data.genres.length; i++){
+             generovacio+=`<article class="myArticles">
+                            <a href="./detalle-genero.html">
+                              <div class="bloque-item-lista">
+                                <h1 class="titulopelis">${ data.genres[i].name}</h1>
+                              </div>
+                            </a>
+                          </article> `
+             console.log(generovacio)
+         }
+         generos.innerHTML = generovacio
         // geneross.innerText+=generovacio[0]
-        geneross.innerText+= data.genres[0].name
-        generos1.innerText+=data.genres[1].name
-        generos2.innerText+=data.genres[2].name
-        generos3.innerText+=data.genres[3].name
-        generos4.innerText+=data.genres[4].name
+        // geneross.innerText+= data.genres[0].name
+        // generos1.innerText+=data.genres[1].name
+        // generos2.innerText+=data.genres[2].name
+        // generos3.innerText+=data.genres[3].name
+        // generos4.innerText+=data.genres[4].name
 
 })
     .catch(function(error) {
